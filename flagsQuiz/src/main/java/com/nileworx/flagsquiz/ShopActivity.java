c13395a8e0621df10388d01d18a65e1a3f6c8796
Jakob2000 <conder408@gmail.com>
2018-04-20 08:25:39 +0200

    implemented videoAd

--- flagsQuiz/src/main/java/com/nileworx/flagsquiz/ShopActivity.java
+++ flagsQuiz/src/main/java/com/nileworx/flagsquiz/ShopActivity.java
@@ -13,7 +13,9 @@ import android.os.SystemClock;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -26,10 +28,23 @@ import com.google.android.gms.ads.AdView;
 
 import java.io.IOException;
 
-
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 
 
 public class ShopActivity extends Activity {
+
+    Button btn_videoAd;
+    private RewardedVideoAd mRewardedVideoAd;
+
+
+
+
+
+
 
     SharedPreferences mSharedPreferences;
     SharedPreferences.Editor e;
@@ -48,6 +63,65 @@ public class ShopActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        btn_videoAd = (Button)findViewById(R.id.btn_videoAd);
+        btn_videoAd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                videoAd();
+            }
+        });
+
+
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
+        mRewardedVideoAd.setRewardedVideoAdListener(new RewardedVideoAdListener() {
+            @Override
+            public void onRewardedVideoAdLoaded() {
+
+            }
+
+            @Override
+            public void onRewardedVideoAdOpened() {
+
+            }
+
+            @Override
+            public void onRewardedVideoStarted() {
+
+            }
+
+            @Override
+            public void onRewardedVideoAdClosed() {
+
+            }
+
+            @Override
+            public void onRewarded(RewardItem rewardItem) {
+
+            }
+
+            @Override
+            public void onRewardedVideoAdLeftApplication() {
+
+            }
+
+            @Override
+            public void onRewardedVideoAdFailedToLoad(int i) {
+
+            }
+
+            @Override
+            public void onRewardedVideoCompleted() {
+
+            }
+        });
+
+
+        loadRewardedVideoAd();
+
+
+
+
 
 
         dialog = new CustomDialog(ShopActivity.this);
@@ -113,36 +187,34 @@ public class ShopActivity extends Activity {
         }
     }
 
-    // ==============================================================================
-    // private void addLevels() {
-    // c = db.getLevels2();
-    //
-    // if (c.getCount() != 0) {
-    //
-    // do {
-    // db.addLevels2(c.getString(c.getColumnIndex("le_country")),
-    // c.getInt(c.getColumnIndex("_leid")));
-    //
-    // } while (c.moveToNext());
-    // }
-    // }
 
-    // ==============================================================================
-    // private void addFlags() {
-    // c = db.getFlags2();
-    //
-    // if (c.getCount() != 0) {
-    //
-    // do {
-    // db.addFlags2(c.getString(c.getColumnIndex("lo_name")),
-    // c.getInt(c.getColumnIndex("lo_level")),
-    // c.getString(c.getColumnIndex("lo_wikipedia")),
-    // c.getString(c.getColumnIndex("lo_info")),
-    // c.getString(c.getColumnIndex("lo_player")),
-    // c.getInt(c.getColumnIndex("_loid")));
-    //
-    // } while (c.moveToNext());
-    // }
-    // }
+
+
+
+
+
+
+
+
+
+
+    public void videoAd(){
+        Log.e("###","videoAd");
+        db.addTotalCoins(1000);
+
+        if (mRewardedVideoAd.isLoaded()) {
+            mRewardedVideoAd.show();
+        }else{
+            Log.e("###","not loaded");
+        }
+
+        loadRewardedVideoAd();
+
+    }
+
+    private void loadRewardedVideoAd() {
+        mRewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917",
+                new AdRequest.Builder().build());
+    }
 
 }
